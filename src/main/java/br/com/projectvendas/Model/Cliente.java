49b0de53269841301d8f88d4f0a0eced2f5496e6
Mario23junior <mariojunior3251@gmail.com>
2021-01-16 02:43:21 -0300

    Criando classes de Modelo para o banco de dados

---
+++ src/main/java/br/com/projectvendas/Model/Cliente.java
@@ -0,0 +1,29 @@
+package br.com.projectvendas.Model;
+
+public class Cliente {
+    
+	private Integer id;
+	private String nome;
+	
+	public Cliente() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
