8ed2e60c87bfacf4d2529d7d024828dffc74160d
Mario23junior <mariojunior3251@gmail.com>
2021-01-16 05:16:33 -0300

    Definido diretiva LAZY na entidade Cliente e corrijindo erro no ItemRepository e renomeando nome de Script sql de criacao de entidades

--- src/main/java/br/com/projectvendas/Repositores/ItemsRepository.java
+++ src/main/java/br/com/projectvendas/Repositores/ItemsRepository.java
@@ -2,6 +2,8 @@ package br.com.projectvendas.Repositores;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ItemsRepository extends JpaRepository<ItemsRepository, Integer> {
+import br.com.projectvendas.Model.ItemPedido;
+
+public interface ItemsRepository extends JpaRepository<ItemPedido, Integer> {
 
 }
