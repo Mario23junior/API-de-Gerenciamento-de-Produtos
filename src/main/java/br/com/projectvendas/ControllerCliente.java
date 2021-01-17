2dd4dfb32028e49a3810e493e4b557405a92f674
Mario23junior <mariojunior3251@gmail.com>
2021-01-17 04:38:53 -0300

    Criando Controller de telemetria de dados e metodo dado da base pelo id

---
+++ src/main/java/br/com/projectvendas/ControllerCliente.java
@@ -0,0 +1,29 @@
+package br.com.projectvendas;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import br.com.projectvendas.Model.Cliente;
+import br.com.projectvendas.Repositores.ClientesRepository;
+
+@RequestMapping("/api/clientes/")
+@RestController
+public class ControllerCliente {
+    
+	 ClientesRepository clientesRepository;
+	
+	 public ControllerCliente(ClientesRepository clientesRepository ) {
+		  this.clientesRepository = clientesRepository;
+	}
+     
+	@GetMapping("/{id}")
+    public Cliente ObterCliente(@PathVariable Integer id) {
+		return clientesRepository
+				           .findById(id)
+				           .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Cliente não encontrado"));
+     }
+}
