ac00b8f555840a1b94bdcc80c91219768df497f4
Mario23junior <mariojunior3251@gmail.com>
2021-01-18 02:04:01 -0300

    diretiva para atualização dos dados recebidos e renomeando end-point delete para DeleteUser

--- src/main/java/br/com/projectvendas/ControllerCliente.java
+++ src/main/java/br/com/projectvendas/ControllerCliente.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -40,12 +41,25 @@ public class ControllerCliente {
 	
 	@DeleteMapping("/id")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public ResponseEntity<?> delete(@PathVariable Integer id) {
+	public ResponseEntity<?> deleteUser(@PathVariable Integer id) {
 		return clientesRepository.findById(id)
 				            .map(deleteUser -> {
 				            clientesRepository.deleteById(id);
 				            return ResponseEntity.ok().build();
 				     }).orElse(ResponseEntity.notFound().build());
+	}
+	
+	
+	@PutMapping
+	public void updateUser(@PathVariable Integer id, @RequestBody Cliente cliente) {
+		    clientesRepository
+		                 .findById(id)
+		                 .map(clienteExistente -> {
+		                	 cliente.setId(clienteExistente.getId());
+		                	 clientesRepository.save(cliente);
+		                	 return ResponseEntity.noContent().build();
+		                 }).orElseGet( () -> ResponseEntity.noContent().build());
+		                 ;
 	}
 	
 }
