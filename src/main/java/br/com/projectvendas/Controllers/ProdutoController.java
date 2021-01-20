938337b4326d2c8a47e8c641813051b32648ad31
Mario23junior <mariojunior3251@gmail.com>
2021-01-20 00:05:52 -0300

    metodo de salvar um novo produto na base de dados

--- src/main/java/br/com/projectvendas/Controllers/ProdutoController.java
+++ src/main/java/br/com/projectvendas/Controllers/ProdutoController.java
@@ -1,14 +1,29 @@
 package br.com.projectvendas.Controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.projectvendas.Model.Produto;
+import br.com.projectvendas.Repositores.ProdutosRepository;
 
 @RestController
 @RequestMapping("/api/produtos")
 public class ProdutoController {
     
+	private ProdutosRepository produtoRepository;
+	
+	public ProdutoController(ProdutosRepository produtosRepository) {
+		this.produtoRepository = produtosRepository;
+ 	}
 	
 	
-	
-	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Produto savePro(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
+	}
 }
