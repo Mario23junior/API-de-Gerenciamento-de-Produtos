a34a4859fffc918c8686f3d451b1909c1949112a
Mario23junior <mariojunior3251@gmail.com>
2021-01-20 00:20:02 -0300

    Criando metodo de atualizar um produtoo na base / alterando nome do controller para seguir padrao

--- src/main/java/br/com/projectvendas/Controllers/ControllerProduto.java
+++ src/main/java/br/com/projectvendas/Controllers/ControllerProduto.java
@@ -1,22 +1,25 @@
 package br.com.projectvendas.Controllers;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.projectvendas.Model.Produto;
 import br.com.projectvendas.Repositores.ProdutosRepository;
 
 @RestController
 @RequestMapping("/api/produtos")
-public class ProdutoController {
+public class ControllerProduto {
     
 	private ProdutosRepository produtoRepository;
 	
-	public ProdutoController(ProdutosRepository produtosRepository) {
+	public ControllerProduto(ProdutosRepository produtosRepository) {
 		this.produtoRepository = produtosRepository;
  	}
 	
@@ -26,4 +29,16 @@ public class ProdutoController {
 	public Produto savePro(@RequestBody Produto produto) {
 		return produtoRepository.save(produto);
 	}
+	
+	@PutMapping("{id}")
+	public void update(@PathVariable Integer id, @RequestBody Produto produto) {
+		produtoRepository
+		           .findById(id)
+		           .map(atualizar -> {
+		        	    produto.setId(atualizar.getId());
+		        	    produtoRepository.save(produto);
+		        	    return produto;
+ 		           }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Produto inserido não encontrado"));
+ 	}
+	
 }
