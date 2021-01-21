8c49b9d313b1b96d9c69a01da6c5f06978ecb691
Mario23junior <mariojunior3251@gmail.com>
2021-01-21 01:13:41 -0300

    Criando metodos de acesso as camadas DTO de itemPedidoDTO e PedidoDTO

--- src/main/java/br/com/projectvendas/Dto/ItemPedidoDTO.java
+++ src/main/java/br/com/projectvendas/Dto/ItemPedidoDTO.java
@@ -1,5 +1,21 @@
 package br.com.projectvendas.Dto;
 
 public class ItemPedidoDTO {
-
+   
+	private Integer produto;
+	private Integer quantidade;
+	public Integer getProduto() {
+		return produto;
+	}
+	public void setProduto(Integer produto) {
+		this.produto = produto;
+	}
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	
 }
--- src/main/java/br/com/projectvendas/Dto/PedidoDTO.java
+++ src/main/java/br/com/projectvendas/Dto/PedidoDTO.java
@@ -1,5 +1,34 @@
 package br.com.projectvendas.Dto;
 
-public class PedidoDTO {
+import java.math.BigDecimal;
+import java.util.List;
 
+public class PedidoDTO {
+    
+	private Integer cliente;
+	private BigDecimal total;
+	private List<ItemPedidoDTO> items;
+	
+	public Integer getCliente() {
+		return cliente;
+	}
+	public void setCliente(Integer cliente) {
+		this.cliente = cliente;
+	}
+	public BigDecimal getTotal() {
+		return total;
+	}
+	public void setTotal(BigDecimal total) {
+		this.total = total;
+	}
+	public List<ItemPedidoDTO> getItems() {
+		return items;
+	}
+	public void setItems(List<ItemPedidoDTO> items) {
+		this.items = items;
+	}
+	
+	
+	
+	
 }
