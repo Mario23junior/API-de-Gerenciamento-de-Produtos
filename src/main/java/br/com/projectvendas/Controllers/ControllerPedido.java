8af373dbaf7785c1b4597674e1c43a255a0865c1
Mario23junior <mariojunior3251@gmail.com>
2021-01-21 00:29:28 -0300

    Ingetando interface PedidoService no Controllerpedido

--- src/main/java/br/com/projectvendas/Controllers/ControllerPedido.java
+++ src/main/java/br/com/projectvendas/Controllers/ControllerPedido.java
@@ -3,8 +3,16 @@ package br.com.projectvendas.Controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.projectvendas.Service.PedidoService;
+
 @RestController
 @RequestMapping("/api/produtos")
 public class ControllerPedido {
-
+      
+	private PedidoService service;
+	
+	public ControllerPedido(PedidoService service) {
+		this.service = service;
+ 	}
+	
 }
---
+++ src/main/java/br/com/projectvendas/Service/implementacoes/PedidoServiceImple.java
@@ -0,0 +1,18 @@
+package br.com.projectvendas.Service.implementacoes;
+
+import org.springframework.stereotype.Service;
+
+import br.com.projectvendas.Repositores.PedidosRepository;
+import br.com.projectvendas.Service.PedidoService;
+
+@Service
+public class PedidoServiceImple implements PedidoService{
+    
+	private PedidosRepository perdidoRepository;
+ 
+	public PedidoServiceImple(PedidosRepository perdidoRepository) {
+		this.perdidoRepository = perdidoRepository;
+	}
+	
+	
+}
