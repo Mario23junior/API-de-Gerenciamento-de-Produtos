3939de6fb6c2a7ec1a4361a414e5d7787ab88fa3
Mario23junior <mariojunior3251@gmail.com>
2021-01-22 00:47:31 -0300

    Criando metodos getter and setter para as informacoes dos items do pedido

--- src/main/java/br/com/projectvendas/Dto/InformacoesItemPedidoDTO.java
+++ src/main/java/br/com/projectvendas/Dto/InformacoesItemPedidoDTO.java
@@ -7,4 +7,25 @@ public class InformacoesItemPedidoDTO {
 	private String descricaoProduto;
 	private BigDecimal precoUnitario;
 	private Integer quantidade;
+	
+	public String getDescricaoProduto() {
+		return descricaoProduto;
+	}
+	public void setDescricaoProduto(String descricaoProduto) {
+		this.descricaoProduto = descricaoProduto;
+	}
+	public BigDecimal getPrecoUnitario() {
+		return precoUnitario;
+	}
+	public void setPrecoUnitario(BigDecimal precoUnitario) {
+		this.precoUnitario = precoUnitario;
+	}
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	
 }
